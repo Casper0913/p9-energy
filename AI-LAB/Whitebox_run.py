81307f42b8d7f0a4a2c8c3108153fde15649f14f
SebxGit <72699667+sebxGit@users.noreply.github.com>
2024-12-30 09:47:28 +0100

    added thetamodel for ai lab

--- AI-LAB/Whitebox_run.py
+++ AI-LAB/Whitebox_run.py
@@ -72,15 +72,15 @@ def save_prediction_and_stats(runtime, config_name, df_predictions, df_true, pre
   df_stats.to_csv(stats_path, index=False)
 
 if __name__ == '__main__':
-  model_name = 'SARIMA'
+  model_name = 'ThetaModel'
   date_start = '2023-11-01'
   date_end = '2024-11-01'
 
   # List of (window_train_size, forecast_horizon, model_config) tuples
   scenarios = [
-    (336, 24, {'order': (2, 1, 2), 'seasonal_order': (2, 2, 2, 12)}),
-    (1440, 336, {'order': (0, 0, 1), 'seasonal_order': (2, 0, 2, 12)}),
-    (17520, 8760, {'order': (0, 0, 0), 'seasonal_order': (1, 1, 2, 12)})
+    (336, 24, {'deseasonalize': False, 'use_test': False, 'method': 'additive', 'difference': True}),
+    (1440, 336, {'deseasonalize': False, 'use_test': True, 'method': 'multiplicative', 'difference': False}),
+    (17520, 8760, {'deseasonalize': False, 'use_test': False, 'method': 'additive', 'difference': True})
   ]
 
   for window_train_size, forecast_horizon, model_config in scenarios:
@@ -109,7 +109,7 @@ if __name__ == '__main__':
 
       data_train_scaled = scaler.fit_transform(data_train[['ConsumptionkWh']])
       data_train = pd.DataFrame(data_train_scaled, columns=['ConsumptionkWh'], index=data_train.index)
-      model = SARIMAX(data_train, order=model_config['order'], seasonal_order=model_config['seasonal_order'])
+      model = ThetaModel(data_train['ConsumptionkWh'], method=model_config['method'], deseasonalize=model_config['deseasonalize'], difference=model_config['difference'])
       try:
         predictions_scaled = forecast_whitebox_model(model, forecast_horizon, model_name)
         predictions = scaler.inverse_transform(predictions_scaled.values.reshape(-1, 1))

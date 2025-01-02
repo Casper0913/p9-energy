6d3004b96e3db88477756aae942b0c1aa6fbac47
Casper Søgård <casper-s@hotmail.dk>
2025-01-02 13:56:17 +0100

    Update model_run.sh

--- AI-LAB/model_run.sh
+++ AI-LAB/model_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=p9-energytransformer_testing
+#SBATCH --job-name=creating_energycontainer
 #SBATCH --output=result_%j.out
 #SBATCH --error=error_%j.err
 #SBATCH --time=03:00:00 # 
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=25G
 
-singularity exec energycontainer.sif python3 BlackBoxTunning.py
+singularity exec energycontainer.sif python3 energycontainer.def
 
  

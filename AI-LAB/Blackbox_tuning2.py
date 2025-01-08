5494fff4c89a2c870a1c0eeb696f8ae8859c87de
SebxGit <72699667+sebxGit@users.noreply.github.com>
2025-01-08 11:41:08 +0100

    Update Blackbox_tuning2.py

--- AI-LAB/Blackbox_tuning2.py
+++ AI-LAB/Blackbox_tuning2.py
@@ -199,7 +199,7 @@ if __name__ == '__main__':
     forecast_horizon = 8760  # hours
     # 336_24, 1440_336, 17520_8760
     trials = 20
-    model_name = f'LSTM_{window_train_size}_{forecast_horizon}'
+    model_name = f'DLinear_{window_train_size}_{forecast_horizon}'
 
     combined_data = loaddataset()
     neuralforecast_data = prepare_neuralforecast_data(combined_data)
@@ -210,7 +210,7 @@ if __name__ == '__main__':
 
     def safe_objective(trial):
         try:
-            return objective_LSTM(trial, data_train, data_test, forecast_horizon)
+            return objective_DLinear(trial, data_train, data_test, forecast_horizon)
         except Exception as e:
             print(f"Failed trial: {e}. Skipped this trial.")
             return float('inf')
